27eed092d7bfe94ac43d25248f5354146228e06b
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2024-11-18 14:45:54 -0800

    updates

--- fneditor/src/main.rs
+++ fneditor/src/main.rs
@@ -2,68 +2,35 @@ use dioxus::prelude::*;
 //use dioxus_logger::tracing::Level; ???
 //use nfd::{open_file_dialog, Response}; // imports file dialog windows for rust cross platform
 
-// declaration of a prop
-#[derive(Props, Clone, PartialEq)] // marcos
-struct CustomProps {
-    text: String,
-    #[props(optional)] // makes props under optional 
-    size: i32
-}
-
-// basic componet
-#[component]
-fn Notes() -> Element {
-    rsx! {
-        "Notes component"
-    }
-}
-
-// testing fuction
-fn test(event: Event<MouseData>) {
-    //println!("{event:?}"); // printing despite type mismatch
-    log::info!("Event thing: {event:?}"); // yes you need a crate called log (A rust universal)
-}
-
-// props with componet
-#[component]
-fn Notes1(props: CustomProps) -> Element {
-    rsx! {
-        p { "{props.text}" }
-    }
-}
 
 fn app() -> Element {
     log::info!("startup log");
-    let mut count = use_signal(||0); // creates new var init with 0 ( HOOK )
 
-    let mut email = use_signal(|| String::from("")); // init with empty sting
+    let mut writen_text = use_signal(|| "");
+    let mut name = use_signal(|| "bob".to_string());
+
     rsx! {
         link { rel: "stylesheet", href: "styles.css" } // styling link
-        p {class: "white", "Testing 1 2 3" } 
-        Notes {}
-        Notes1 {text: "test from  struct"}
-
-        // button tied to event handeler
-        button {
-            onclick: move |event | { 
-                test(event);
-                count+=1;
-            },
-            "Click me!"
-        }
-        p {class: "white", "Clicked : {count}"}
-
-        // User input
-        input {
-            value: "{email}",
-            oninput: move |event| {
-                email.set(event.value())
-            }
-        }
-        p {class: "white","User input : {email}"}
         div {
             class:"container",
+
+            p {"text: {writen_text}"},
+            /* 
+            button {
+                "Open"
+                oninput: move |_event| {
+                }
+            }
+            button {
+                "Save"
+                oninput: move |_event| {
+                }
+            }
+            */
             textarea {
+                oninput: move |event| {
+                    writen_text.set(event.value());
+                },
                 class:"textarea"
             }
         }
