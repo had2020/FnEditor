d5424967590e2a706f9151c2012d4c9c5cfa94f3
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2024-11-18 15:31:23 -0800

    Update main.rs

--- fneditor/src/main.rs
+++ fneditor/src/main.rs
@@ -2,36 +2,59 @@ use dioxus::prelude::*;
 //use dioxus_logger::tracing::Level; ???
 //use nfd::{open_file_dialog, Response}; // imports file dialog windows for rust cross platform
 
+fn save_file() {
+    log::info!("save");
+}
+
+fn open_file() {
+    log::info!("save");
+}
 
 fn app() -> Element {
     log::info!("startup log");
 
-    let mut writen_text = use_signal(|| "");
-    let mut name = use_signal(|| "bob".to_string());
+    let mut array_strings = use_signal(|| Vec<String> = vec!)
+    let mut writen_text = use_signal(|| "".to_string());
+    let mut filename = use_signal(|| "".to_string());
 
     rsx! {
         link { rel: "stylesheet", href: "styles.css" } // styling link
-        div {
-            class:"container",
 
-            p {"text: {writen_text}"},
-            /* 
-            button {
-                "Open"
-                oninput: move |_event| {
+        div {
+            class: "container",
+
+            div {
+                class: "topnav",
+            
+                p {"text: {writen_text}"}
+
+                button {
+                    onclick: move |_event| {
+                        open_file();
+                    },
+                    "Open",
+                }
+    
+                button {
+                    onclick: move |_event| {
+                        save_file();
+                    },
+                    "Save",
+                }
+    
+                input {
+                    oninput: move |event| {
+                        filename.set(event.value());
+                    },
+                    "filename",
                 }
             }
-            button {
-                "Save"
-                oninput: move |_event| {
-                }
-            }
-            */
+
             textarea {
                 oninput: move |event| {
                     writen_text.set(event.value());
                 },
-                class:"textarea"
+                class: "textarea"
             }
         }
     }
