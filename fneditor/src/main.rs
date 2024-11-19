96a6b2eb292261d1a4e24d08684ccf544db8aa62
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2024-11-19 10:44:11 -0800

    Update main.rs

--- fneditor/src/main.rs
+++ fneditor/src/main.rs
@@ -2,8 +2,17 @@ use dioxus::prelude::*;
 //use dioxus_logger::tracing::Level; ???
 //use nfd::{open_file_dialog, Response}; // imports file dialog windows for rust cross platform
 
+//use std::fs::File;
+//use std::io::Write;
+
+use std::io::Read;
+use web_sys::FileReader;
+
+
 fn save_file() {
     log::info!("save");
+    //let mut file = File::create("example.txt").expect("Unable to create file");
+    //file.write_all(b"Hello, World!\n").expect("Unable to write to file");
 }
 
 fn open_file() {
@@ -13,9 +22,12 @@ fn open_file() {
 fn app() -> Element {
     log::info!("startup log");
 
-    let mut array_strings = use_signal(|| Vec<String> = vec!)
-    let mut writen_text = use_signal(|| "".to_string());
+    //let mut text_array: Vec<String> = vec! [String::new(); 126];
+    let mut input_key = use_signal(|| "".to_string());
     let mut filename = use_signal(|| "".to_string());
+    let mut written_text = use_signal(|| "".to_string());
+
+    let mut filenames: Signal<Vec<String>> = use_signal(Vec::new);
 
     rsx! {
         link { rel: "stylesheet", href: "styles.css" } // styling link
@@ -25,15 +37,6 @@ fn app() -> Element {
 
             div {
                 class: "topnav",
-            
-                p {"text: {writen_text}"}
-
-                button {
-                    onclick: move |_event| {
-                        open_file();
-                    },
-                    "Open",
-                }
     
                 button {
                     onclick: move |_event| {
@@ -48,11 +51,45 @@ fn app() -> Element {
                     },
                     "filename",
                 }
+
+                input {
+                    // tell the input to pick a file
+                    r#type: "file",
+                    // list the accepted extensions
+                    accept: ".txt,.rs",
+                    // pick multiple files
+                    multiple: false,
+                    onchange: move |evt| {
+                        if let Some(file_engine) = &evt.files() {
+                            let files = file_engine.files();
+                            for file_name in files {
+                                filenames.write().push(file_name);
+                            }
+                        }
+                    }
+                }
+
+                button {
+                    onclick: move |_event| {
+                        open_file();
+                    },
+                    "Open",
+                }
+
             }
 
             textarea {
+                
                 oninput: move |event| {
-                    writen_text.set(event.value());
+                    written_text.set(event.value());
+                },
+
+                onkeydown: move |event| {
+                    if event.key().to_string() == "Enter" {
+                        input_key.set(event.key().to_string());
+                        //text_array.push(written_line());
+                        written_text.set(written_text() + "\n")
+                    }
                 },
                 class: "textarea"
             }
