edf7384733ab5d95a9f29668990a4599abad643e
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2024-11-20 21:33:35 -0800

    update

--- fneditor/src/main.rs
+++ fneditor/src/main.rs
@@ -1,112 +1,40 @@
-use dioxus::prelude::*;
-//use dioxus_logger::tracing::Level; ???
-//use nfd::{open_file_dialog, Response}; // imports file dialog windows for rust cross platform
+use gpui::*;
 
-//use std::fs::File;
-//use std::io::Write;
-
-//use std::io::Read;
-
-use web_sys::*;
-use web_sys::FileReader;
-
-
-fn save_file(file_name: &str, content: &str) {
-    let window = web_sys::window().unwrap();
-    let blob = Blob::new_with_str(content).unwrap();
-    let url = Url::create_object_url_with_blob(&blob).unwrap();
-    window.open_with_url_and_target(&url, "_blank").unwrap();
+struct HelloWorld {
+    text: SharedString,
 }
 
-
-fn open_file() {
-    log::info!("save");
-}
-
-fn app() -> Element {
-    log::info!("startup log");
-
-    //let mut text_array: Vec<String> = vec! [String::new(); 126];
-    let mut input_key = use_signal(|| "".to_string());
-    let mut filename = use_signal(|| "".to_string());
-    let mut written_text = use_signal(|| "".to_string());
-
-    let mut file_data = use_signal(|| "".to_string());
-
-    rsx! {
-        link { rel: "stylesheet", href: "styles.css" } // styling link
-
-        div {
-            class: "container",
-
-            p {}
-
-            div {
-                class: "topnav",
-    
-                button {
-                    onclick: move |_event| {
-                        save_file();
-                    },
-                    "Save",
-                }
-    
-                input {
-                    oninput: move |event| {
-                        filename.set(event.value());
-                    },
-                    "filename",
-                }
-
-
-                input {
-                    // tell the input to pick a file
-                    r#type: "file",
-                    // list the accepted extensions
-                    accept: ".txt,.rs",
-                    // pick multiple files
-                    multiple: false,
-                    onchange: move |evt| {
-                        if let Some(file_engine) = &evt.files() {
-                            let files = file_engine.files();
-                            for file in files {
-                                let file_reader = FileReader::new().unwrap();
-                                let file_content = file_reader.as_string().unwrap();
-                                file_data.set(file_content)
-                            }
-                        }
-                    }
-                }
-
-                button {
-                    onclick: move |_event| {
-                        open_file();
-                    },
-                    "Open",
-                }
-
-            }
-
-            textarea {
-                
-                oninput: move |event| {
-                    written_text.set(event.value());
-                },
-
-                onkeydown: move |event| {
-                    if event.key().to_string() == "Enter" {
-                        input_key.set(event.key().to_string());
-                        //text_array.push(written_line());
-                        written_text.set(written_text() + "\n")
-                    }
-                },
-                class: "textarea"
-            }
-        }
+impl Render for HelloWorld {
+    fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
+        div()
+            .flex()
+            .bg(rgb(0x2e7d32))
+            .size(Length::Definite(Pixels(300.0).into()))
+            .justify_center()
+            .items_center()
+            .shadow_lg()
+            .border_1()
+            .border_color(rgb(0x0000ff))
+            .text_xl()
+            .text_color(rgb(0xffffff))
+            .child(format!("Hello, {}!", &self.text))
     }
 }
 
 fn main() {
-    launch(app)
+    App::new().run(|cx: &mut AppContext| {
+        let bounds = Bounds::centered(None, size(px(300.0), px(300.0)), cx);
+        cx.open_window(
+            WindowOptions {
+                window_bounds: Some(WindowBounds::Windowed(bounds)),
+                ..Default::default()
+            },
+            |cx| {
+                cx.new_view(|_cx| HelloWorld {
+                    text: "World".into(),
+                })
+            },
+        )
+        .unwrap();
+    });
 }
-
