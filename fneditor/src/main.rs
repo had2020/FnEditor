ecae2ec18e389d447eabde76e422152cc1d6343b
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-01-30 13:19:15 -0800

    Update main.rs

--- fneditor/src/main.rs
+++ fneditor/src/main.rs
@@ -3,33 +3,39 @@ use laststraw::*;
 fn main() {
     let mut app = App::new(500, 500, "test");
 
+    let mut text_position_offset: f32 = 0.0;
+
     asx!({
-        set_window_color(&mut app, "Obsidian"); // is layored so this is back
+        set_window_color(&mut app, "Obsidian");
 
         if input_pressed(&app, "esc") {
             app.should_close = true;
         }
 
+        // basic scrolling
+        if input_pressed(&app, "up") {
+            text_position_offset -= 5.0;
+        }
+
+        if input_pressed(&app, "down") {
+            text_position_offset += 5.0;
+        }
+
+        let new_y_pos: f32 = 85.0 + text_position_offset;
+        let editable_position: Position = position!(10.0, new_y_pos, 30.0);
+        let texty = editable_lines(&mut app, editable_position, "enter:", "White", false);
+
         set_next_button(&mut app, position!(30.0, 30.0, 30.0)); // maybe wrap as struct
         set_next_button_text(&mut app, "SAVE");
         button!({
-            println!("save");
+            println!("saved");
         });
 
         set_next_button(&mut app, position!(100.0, 30.0, 30.0)); // maybe wrap as struct
         set_next_button_text(&mut app, "LOAD");
         button!({
-            println!("load");
+            println!("loaded");
         });
-
-        let texty = editable_lines(
-            &mut app,
-            position!(100.0, 50.0, 50.0),
-            "enter:",
-            "Blue",
-            false,
-        );
-        single_line_text(&mut app, position!(0.0, 30.0, 40.0), &texty); // you can acess the value later, will be empty, never recived input
 
         limit_fps(&mut app, 60.0);
     });
