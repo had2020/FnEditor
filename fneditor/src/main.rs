c1529cee5a67a78809c9edbaf26b18c156407d90
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-01-30 15:32:14 -0800

    Update main.rs

--- fneditor/src/main.rs
+++ fneditor/src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     let mut app = App::new(500, 500, "test");
 
     let mut text_position_offset: f32 = 0.0;
+    let mut file_path: String = String::new();
 
     asx!({
         set_window_color(&mut app, "Obsidian");
@@ -23,19 +24,15 @@ fn main() {
 
         let new_y_pos: f32 = 85.0 + text_position_offset;
         let editable_position: Position = position!(10.0, new_y_pos, 30.0);
-        let texty = editable_lines(&mut app, editable_position, "enter:", "White", false);
+        let texty = editable_lines(&mut app, editable_position, "text:", "White", false);
 
-        set_next_button(&mut app, position!(30.0, 30.0, 30.0)); // maybe wrap as struct
-        set_next_button_text(&mut app, "SAVE");
-        button!({
-            println!("saved");
-        });
-
-        set_next_button(&mut app, position!(100.0, 30.0, 30.0)); // maybe wrap as struct
-        set_next_button_text(&mut app, "LOAD");
-        button!({
-            println!("loaded");
-        });
+        let path = editable_lines(
+            &mut app,
+            position!(10.0, 30.0, 30.0),
+            "path:",
+            "White",
+            true,
+        );
 
         limit_fps(&mut app, 60.0);
     });
